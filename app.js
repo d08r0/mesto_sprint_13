275dc82ca20b79b00980a7f3615f06c5f74a9392
scrooge <etoerik@gmail.com>
2020-08-13 01:48:26 +0300

    Add mongoose

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const usersRouter = require('./routes/users.js');
 const cardsRouter = require('./routes/cards.js');
 const userRouter = require('./routes/user.js');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mydb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.use(express.static(`${__dirname}/public`));
 
